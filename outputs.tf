39f54fd6504e20b73a57d2dcd6e639fdd69328a6
Paul Canham <paul.canham@cambridge.org>
2024-06-30 20:36:15 +0100

    chore(lint): List items should be accessed using square brackets

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "bastion_eip" {
-  value = aws_eip.bastion_ip.*.public_ip
+  value = aws_eip.bastion_ip.[*].public_ip
 }
 
 output "bastion_secgrpid" {
-  value = aws_security_group.bastion_security_group.*.id
+  value = aws_security_group.bastion_security_group.[*].id
 }
