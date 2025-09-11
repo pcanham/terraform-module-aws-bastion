4889d62447277ab11ce0109b2a90572e93e59e59
pscanham <paul@canham.io>
2025-09-11 12:10:11 +0000

    fix: add exclusion of shellcheck

--- user-data.sh
+++ user-data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
 INSTANCEID=$(curl -s -m 60 http://169.254.169.254/latest/meta-data/instance-id)
+# shellcheck disable=SC2154
 aws --region "${region}" ec2 associate-address --instance-id "${INSTANCEID}" --allocation-id "${eip-allocation-id}"
